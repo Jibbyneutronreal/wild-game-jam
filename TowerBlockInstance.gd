b321070d9893c18e38af54c87513490908b07c2c
RandomKidWhoIsLearningPython <119384741+RandomKidWhoIsLearningPython@users.noreply.github.com>
2025-03-15 12:24:33 -0400

    changes
    added tower spawning

--- TowerBlockInstance.gd
+++ TowerBlockInstance.gd
@@ -1,15 +1,36 @@
 extends CharacterBody2D
 
 var tower_block = preload("res://Block.tscn")
+var last_block_y = 680  # Tracks the y position of the last spawned block
+var block_gap = 125
+var grav = 10
+@onready var camera = %Camera2D
+var numberBlocks = 5
 
 func _ready():
-	blockinstance()
+	position.x = 480
 func blockinstance():
+	if numberBlocks > 0:
+		var tower_block_instance = tower_block.instantiate()  
+		
+		# Set position to be above the last block
+		tower_block_instance.position = Vector2(100, last_block_y - block_gap)  # Adjust X and Y as needed
 
-	var tower_block_instance = tower_block.instantiate()  
-	get_tree().root.add_child(tower_block_instance)
-	print("instanced")
+		# Add the block to the scene
+		get_tree().root.add_child(tower_block_instance)
+		
+		# Update last block's Y position
+		last_block_y = tower_block_instance.position.y
+		numberBlocks -=1
+	
+func _process(_delta):
+	if numberBlocks < 0:
+		numberBlocks = 0
 
-func _process(_delta) -> void:
-	if Input.is_action_just_pressed("LeftClick"):
+	if Input.is_action_just_pressed("spawnBlock"):
+		print("spawned block")
 		blockinstance()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	camera.position.y = 350
---
+++ camera_2d.gd
@@ -0,0 +1,10 @@
+extends Camera2D
+
+
+#func _ready():
+	#position.x = 576
+	#position.y = 500
+#
+#func _physics_process(delta):
+	#if Input.is_action_just_pressed("spawnBlock"):
+		#position.y -= 125
