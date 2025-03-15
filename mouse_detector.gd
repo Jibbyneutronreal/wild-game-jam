bbe37f142f2d302ea919c99f3f210d12769f393b
Jibbyneutronreal <jibril.tanvir@gmail.com>
2025-03-15 18:40:45 -0400

    Update mouse_detector.gd

--- mouse_detector.gd
+++ mouse_detector.gd
@@ -3,17 +3,20 @@ extends Area2D
 var MouseInArea = false
 var Dragging = false
 
+
 func _process(delta):
 	if MouseInArea == true:
 		if Input.is_action_pressed("LeftClick"):
 			global_position = get_global_mouse_position()
+			Dragging = true
+			print("dragging")
 		elif Input.is_action_just_released("LeftClick"):
-			pass
+			Dragging = false
 
 func _on_mouse_entered() -> void:
 	print("mouse on block")
-	var MouseInArea = true
+	MouseInArea = true
 
 func _on_mouse_exited() -> void:
 	print("mouse exited block")
-	var MouseInArea = false
+	MouseInArea = false
