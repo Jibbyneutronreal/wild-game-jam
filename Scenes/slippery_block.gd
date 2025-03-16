c9ed42a04089b4341a5b5a47e1c41e16c238b6c6
Jibbyneutronreal <jibril.tanvir@gmail.com>
2025-03-16 16:02:39 -0400

    added slippery scene it doesent drag too lazy to fix

---
+++ Scenes/slippery_block.gd
@@ -0,0 +1,30 @@
+extends RigidBody2D
+
+var tower_block = preload("res://Scenes/Slippery_block.tscn")
+var last_block_y = 680
+var block_gap = 125
+var grav = 300
+var numberBlocks = 5
+var is_stuck = false
+var stick_to_body = null
+
+func _ready():
+	# These properties are valid for RigidBody2D
+	contact_monitor = true
+	max_contacts_reported = 10
+
+func blockinstance():
+	if numberBlocks > 0:
+		var tower_block_instance = tower_block.instantiate()  
+		get_tree().root.add_child(tower_block_instance)
+		tower_block_instance.global_position = Vector2(global_position.x, last_block_y - block_gap)
+		last_block_y -= block_gap
+		numberBlocks -= 1
+
+func _process(delta):
+	if numberBlocks < 0:
+		numberBlocks = 0
+
+func _physics_process(delta):
+	if not is_stuck:
+		position.y += grav * delta
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 @onready var block = preload("res://Scenes/Block.tscn")
 @onready var sticky_block = preload("res://Scenes/sticky_block.tscn")
+@onready var slippery_block = preload("res://Scenes/Slippery_block.tscn")
 
 func spawn_block():
 	var block_scence = block.instantiate()
@@ -8,14 +9,22 @@ func spawn_block():
 	add_child(block_scence)
 	
 	
-func spawn_stick():
+func spawn_sticky():
 	var sticky_scene = sticky_block.instantiate()
 	sticky_scene.position.x = randf_range(100, 1152)
 	add_child(sticky_scene)
+	
+	
+func spawn_slippery():
+	var slippery_scene = slippery_block.instantiate()
+	slippery_scene.position.x = randf_range(100, 1152)
+	add_child(slippery_scene)
 	
 	
 func _physics_process(delta):
 	if Input.is_action_just_pressed("spawnBlock"):
 		spawn_block()
 	if Input.is_action_just_pressed("spawnSticky"):
-		spawn_stick()
+		spawn_sticky()
+	if Input.is_action_just_pressed("spawnSlippery"):
+		spawn_slippery()
