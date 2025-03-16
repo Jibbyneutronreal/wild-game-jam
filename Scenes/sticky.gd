9be25a4702738c7b3a89d3734ee6b96c5b1b2275
Jibbyneutronreal <jibril.tanvir@gmail.com>
2025-03-16 15:14:32 -0400

    cooked (could not fix the sticky block)

--- Scenes/sticky.gd
+++ Scenes/sticky.gd
@@ -1,36 +1,19 @@
 extends Area2D
 
-var is_stuck = false  # Track if the sticky block has attached
-var pin_joint : PinJoint2D = null  # Variable to store the PinJoint2D
+func _on_sticky_block_body_entered(body):
+	if body.is_in_group("blocks"):
+		if body.is_in_group("Sticked"):
+			pass
+		else:
+			attach_to_block(body)
 
-# Called when another object (body) enters the area of this block
-func _on_body_entered(body):
-	print("Collided with: ", body.name)
-	if not is_stuck and body is RigidBody2D:
-		stick_to_block(body)
+func attach_to_block(block):
+	call_deferred("_attach_block_deferred", block)
 
-# Called when another area enters the area of this block
-func _on_area_entered(area):
-	print("Entered area of: ", area.name)
-	if not is_stuck and area is RigidBody2D:
-		stick_to_block(area)
-
-# Stick the block to the target block
-func stick_to_block(target_block):
-	if target_block is RigidBody2D and not is_stuck:
-		# Create a PinJoint2D to keep the blocks together
-		pin_joint = PinJoint2D.new()
-		pin_joint.position = global_position  # Attach at the same position as this block
-		pin_joint.node_a = get_path()  # This (sticky) block
-		pin_joint.node_b = target_block.get_path()  # Target block
+func _attach_block_deferred(block):
+	if block.is_inside_tree():
+		block.get_parent().remove_child(block)
+		add_child(block)
+		block.position = self.to_local(block.global_position)
+		block.add_to_group("Sticked")
 		
-		# Add the joint to the parent node (the container of the blocks)
-		get_parent().add_child(pin_joint)  
-		
-		# Mark the block as stuck
-		is_stuck = true
-		
-		# Optionally adjust position of the sticky block relative to the target block
-		self.position = target_block.position  # Align sticky block with target block
-
-		# Set target block to Kinematic mode to prevent it from moving independently
--- Scripts/TowerBlockInstance.gd
+++ Scripts/TowerBlockInstance.gd
@@ -12,13 +12,6 @@ func _ready():
 	# These properties are valid for RigidBody2D
 	contact_monitor = true
 	max_contacts_reported = 10
-	
-	# Get the sticky detection area and connect its signal
-	var sticky_area = $sticky_detection
-	if sticky_area:
-		sticky_area.connect("body_entered", Callable(self, "_on_sticky_detection_body_entered"))
-	else:
-		print("Error: sticky_detection node not found!")
 
 func blockinstance():
 	if numberBlocks > 0:
@@ -35,23 +28,3 @@ func _process(delta):
 func _physics_process(delta):
 	if not is_stuck:
 		position.y += grav * delta
-
-# Called when the sticky area detects a collision
-func _on_sticky_detection_body_entered(body):
-	# Check if the colliding body is another block or something we want to stick to
-	if body != self and not is_stuck and body is RigidBody2D:
-		print("Sticking to: ", body.name)
-		is_stuck = true
-		stick_to_body = body
-		
-		# Create a joint to connect the bodies
-		var joint = PinJoint2D.new()
-		joint.name = "StickJoint"
-		add_child(joint)
-		joint.global_position = global_position
-		joint.node_a = get_path()
-		joint.node_b = body.get_path()
-		
-		# Make the joint stiff
-		joint.bias = 0.5  # Using 0.5 instead of 0.9 for more stability
-		joint.disable_collision = true
