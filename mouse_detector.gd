4b9a036e68ef3ea97ee4df7fd792b2453a6cf23a
RandomKidWhoIsLearningPython <119384741+RandomKidWhoIsLearningPython@users.noreply.github.com>
2025-03-15 20:45:11 -0400

    drag
    drag

--- mouse_detector.gd
+++ mouse_detector.gd
@@ -1,22 +1,25 @@
 extends Area2D
 
-var MouseInArea = false
-var Dragging = false
+var dragging = false
 
+@onready var tower_block = $".."
 
-func _process(delta):
-	if MouseInArea == true:
-		if Input.is_action_pressed("LeftClick"):
-			global_position = get_global_mouse_position()
-			Dragging = true
-			print("dragging")
-		elif Input.is_action_just_released("LeftClick"):
-			Dragging = false
+var clickable = false
 
-func _on_mouse_entered() -> void:
-	print("mouse on block")
-	MouseInArea = true
+func _process(_delta):
+	if dragging:
+		tower_block.global_position = get_global_mouse_position()
 
-func _on_mouse_exited() -> void:
-	print("mouse exited block")
-	MouseInArea = false
+func _input(event):
+	if Input.is_action_pressed("holdItem") and clickable:
+		dragging = true
+	elif Input.is_action_just_released("holdItem") and clickable:
+		dragging = false
+
+func _on_mouse_entered():
+	print("Mouse entered block")
+	clickable = true
+
+func _on_mouse_exited():
+	print("Mouse exited block")
+	clickable = false
