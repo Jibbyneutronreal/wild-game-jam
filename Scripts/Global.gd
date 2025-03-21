cd65b6a2f560501bfa58bdeadb9d347888e8f1a6
RandomKidWhoIsLearningPython <119384741+RandomKidWhoIsLearningPython@users.noreply.github.com>
2025-03-21 07:38:55 -0400

    654

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,1 +1,6 @@
 extends Node
+
+
+
+var coins = starting_coins
+var starting_coins = 1000
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -6,23 +6,33 @@ extends Node2D
 @onready var metal_block = preload("res://Scenes/metal_block.tscn")
 @onready var gamet = $gamet
 @onready var time = $text/time
+@onready var coins = $text/coins
+
+
+func _ready():
+	g.coins = g.starting_coins
+
 
 func spawn_block():
-	# Spawn a standard or slippery block based on a random chance
-	var random_number = randi() % 10 + 1
-	print("Random number: ", random_number)
-	if random_number >= 7:
-		spawn_slippery()
-	else:
-		var block_scene = block.instantiate()
-		block_scene.position.x = randf_range(100, 1152)
-		add_child(block_scene)
+	if g.coins >= 10:
+		# Spawn a standard or slippery block based on a random chance
+		var random_number = randi() % 10 + 1
+		print("Random number: ", random_number)
+		if random_number >= 7:
+			spawn_slippery()
+		else:
+			var block_scene = block.instantiate()
+			block_scene.position.x = randf_range(100, 1152)
+			add_child(block_scene)
+		g.coins -= 10
 
 func spawn_sticky():
-	# Spawn a sticky block
-	var sticky_scene = sticky_block.instantiate()
-	sticky_scene.position.x = randf_range(100, 1152)
-	add_child(sticky_scene)
+	if g.coins >= 20:
+		# Spawn a sticky block
+		var sticky_scene = sticky_block.instantiate()
+		sticky_scene.position.x = randf_range(100, 1152)
+		add_child(sticky_scene)
+	g.coins -= 20
 
 func spawn_slippery():
 	# Spawn a slippery block
@@ -31,12 +41,15 @@ func spawn_slippery():
 	add_child(slippery_scene)
 
 func spawn_metal():
-	# Spawn a shield block
-	var metal_scene = metal_block.instantiate()
-	metal_scene.position.x = randf_range(100, 1152)
-	add_child(metal_scene)
+	if g.coins >= 20:
+		# Spawn a shield block
+		var metal_scene = metal_block.instantiate()
+		metal_scene.position.x = randf_range(100, 1152)
+		add_child(metal_scene)
+		g.coins -= 20
 
 func _physics_process(delta):
+	coins.text = "Coins: " + str(g.coins) 
 	# Handle block spawning based on input actions
 	if Input.is_action_just_pressed("spawnBlock"):
 		spawn_block()
@@ -44,11 +57,10 @@ func _physics_process(delta):
 		spawn_sticky()
 	if Input.is_action_just_pressed("spawnShield"):
 		spawn_metal()
-	if Input.is_action_just_pressed("spawnSlippery"):
-		spawn_slippery()
-
 	# Update the text to display the time remaining
 	time.text = str(int(gamet.time_left))
+	if g.coins <= 0:
+		g.coins = 0
 
 func _on_gamet_timeout():
 	# Reload the current scene when the timer runs out
